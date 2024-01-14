dc740d3c3ea8eb53d63362e2356847611f7931b2
Nilanchal <nilanchalpanda2003@gmail.com>
2024-01-14 21:24:53 +0530

    Change navbar

--- REACT_REDUX/src/components/Navbar.jsx
+++ REACT_REDUX/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
 
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const selector = useSelector((state) => state.cart);
@@ -9,7 +9,11 @@ const Navbar = () => {
   return (
     <div className="shadow-xl shadow-slate-900 bg-tertiary text-2xl py-5 flex justify-between px-5 md:justify-between md:px-24 items-center font-mono">
       <Link to="/">
-        <img src="./Logo.png" alt="Logo" className="w-[50px] mix-blend-normal"/>
+        <img
+          src="./Logo.png"
+          alt="Logo"
+          className="w-[50px] mix-blend-normal"
+        />
       </Link>
 
       <div className="flex items-center text-lg gap-x-4 md:gap-x-8">
@@ -20,15 +24,12 @@ const Navbar = () => {
           Home
         </Link>
         <Link
-          className="text-secondary hover:text-secondaryhover text-inherit"
           to="/wishlist"
+          className="flex justify-center items-center w-10 h-10 relative rounded-full bg-secondaryhover"
         >
-          WishList
-        </Link>
-        <span className="relative p-2 rounded-full bg-secondaryhover">
           <FaHeart />
-          <span className="absolute -top-3 right-0">{selector.length}</span>
-        </span>
+          <span className="absolute right-1 -top-2">{selector.length}</span>
+        </Link>
       </div>
     </div>
   );
