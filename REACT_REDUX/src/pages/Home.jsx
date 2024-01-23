cb4dc75728b123de1092f7524ae0d81249e75bda
Nilanchal <nilanchalpanda2003@gmail.com>
2024-01-23 23:29:38 +0530

    Changed hero part

--- REACT_REDUX/src/pages/Home.jsx
+++ REACT_REDUX/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="mx-10 mt-10 md:mx-10">
-      <section className="flex flex-col md:flex-row justify-evenly pt-5 px-2 pb-0 bg-secondary/20 rounded-3xl md:p-10 md:pb-0">
+      <section className="flex flex-col md:flex-row justify-evenly pt-5 px-2 pb-0 bg-secondary/20 rounded-3xl md:mx-20 md:p-10 md:pb-0">
         <h1 className="flex items-center justify-center text-5xl font-black leading-[125%] sm:text-5xl md:text-[96px] md:leading-[125%]">
           LET’S
           <br /> EXPLORE <br />
