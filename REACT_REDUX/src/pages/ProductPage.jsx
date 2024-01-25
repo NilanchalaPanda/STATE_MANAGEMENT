0e43dd102b625db572c4a4fe6bc7d61e9704e1a8
Nilanchal <nilanchalpanda2003@gmail.com>
2024-01-25 23:25:52 +0530

    Single Product Page bug fixed

--- REACT_REDUX/src/pages/ProductPage.jsx
+++ REACT_REDUX/src/pages/ProductPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
 import { add } from "../store/cartSlice";
@@ -52,17 +53,17 @@ const ProductPage = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 my-10 mx-3 md:my-20">
       <img
-        src={product.image}
-        alt={product.title}
+        src={product?.image}
+        alt={product?.title}
         className="h-[350px] md:h-[600px] mx-auto rounded-3xl "
       />
 
       <div className="mt-5 flex flex-col justify-start gap-y-5 mx-5">
-        <h1 className="text-2xl font-bold">{product.title}</h1>
-        <p className="text-gray-400 -mt-5">{product.description}</p>
+        <h1 className="text-2xl font-bold">{product?.title}</h1>
+        <p className="text-gray-400 -mt-5">{product?.description}</p>
 
         <div className="flex items-center justify-around">
-          <span className="font-bold text-2xl">${product.price}</span>
+          <span className="font-bold text-2xl">${product?.price}</span>
           <button
             onClick={() => handleAdd(product)}
             className="py-2 px-4 bg-red-700 rounded-2xl border border-slate-300"
